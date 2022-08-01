18f79dab9a09eb89904f1a2e82f3f6d044581936
vapatty6848 <vapatty@gmail.com>
2022-08-01 14:07:34 -0300

    adicionando stories - usando como base storybook Rocketseat

---
+++ my-story/src/components/index.tsx
@@ -0,0 +1,6 @@
+export { Button } from './Button';
+export { Separator } from './Separator';
+export { Input } from './Input';
+export { Radio } from './Radio';
+export { CheckBox } from './CheckBox';
+export { Modal } from './Modal';
---
+++ my-story/src/stories/Input.stories.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { storiesOf } from '@storybook/react';
+
+import { MdSearch } from 'react-icons/md';
+import { Input } from '../components';
+
+storiesOf('Input', module)
+  .add('Default', () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+      <Input
+        name="rocketseat"
+        placeholder="Rocketseat..."
+        value={inputValue}
+        setValue={setInputValue}
+      />
+    );
+  })
+  .add('With Icon', () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+      <Input
+        name="rocketseat"
+        icon={<MdSearch />}
+        placeholder="Rocketseat..."
+        value={inputValue}
+        setValue={setInputValue}
+      />
+    );
+  });
